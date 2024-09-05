3280e071bb36775eaad0d07e55678e04f47f4863
Mr Whipp <dwhipp88@gmail.com>
2024-09-05 17:10:52 -0600

    completed lesson 236, CREATING POST, PUT, AND PATCH ROUTES

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const port = 3000;
 const masterKey = '4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT';
 
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.urlencoded({ extended: true }));
 
 //1. GET a random joke
 app.get('/', (req, res) => {
@@ -25,26 +24,51 @@ app.get('/filter', (req, res) => {
   const filteredJokeType = jokes.filter(
     (filteredJoke) => filteredJoke.jokeType === req.query.jokeType
   );
-  console.log('FILTERD JOKE: ', filteredJokeType);
   res.send(filteredJokeType);
 });
 
 //4. POST a new joke
 app.post('/jokes', (req, res) => {
+  // console.log('Original Length: ', jokes.length);
   const newJoke = {
     id: Math.floor(Math.random() * 10000000000),
     jokeText: req.body.text,
     jokeType: req.body.type,
   };
-  // console.log('Req Body: ', req.body);
-  // console.log('User Post: ', newJoke);
   jokes.push(newJoke);
+  console.log('Jokes: ', jokes.slice(-1));
   res.json(newJoke);
 });
 
 //5. PUT a joke
+app.put('/jokes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+
+  jokes[searchIndex] = replacementJoke;
+  res.json(replacementJoke);
+});
 
 //6. PATCH a joke
+app.patch('/jokes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const existingJoke = jokes.find((joke) => joke.id === id);
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text || existingJoke.jokeText,
+    jokeType: req.body.type || existingJoke.jokeType,
+  };
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  jokes[searchIndex] = replacementJoke;
+  console.log(jokes[searchIndex]);
+  res.json(replacementJoke);
+});
 
 //7. DELETE Specific joke
 
